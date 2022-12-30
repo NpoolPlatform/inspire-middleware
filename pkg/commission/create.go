5cf05e2315cbdac16adea464000e2945ded3731e
Zhao KK <kikakkz@hotmail.com>
2022-12-30 10:37:57 +0000

    Add good order percent

--- pkg/commission/create.go
+++ pkg/commission/create.go
@@ -2,10 +2,22 @@ package commission
 
 import (
 	"context"
+	"fmt"
 
+	mgrpb "github.com/NpoolPlatform/message/npool/inspire/mgr/v1/commission"
 	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/commission"
+
+	goodorderpercent "github.com/NpoolPlatform/inspire-middleware/pkg/commission/goodorderpercent"
 )
 
 func CreateCommission(ctx context.Context, in *npool.CommissionReq) (*npool.Commission, error) {
-	return nil, nil
+	switch in.GetSettleType() {
+	case mgrpb.SettleType_GoodOrderPercent:
+		return goodorderpercent.CreateGoodOrderPercent(ctx, in)
+	case mgrpb.SettleType_LimitedOrderPercent:
+	case mgrpb.SettleType_AmountThreshold:
+	default:
+		return nil, fmt.Errorf("unknown settle type")
+	}
+	return nil, fmt.Errorf("not implemented")
 }
---
+++ pkg/commission/goodorderpercent/create.go
@@ -0,0 +1,11 @@
+package goodorderpercent
+
+import (
+	"context"
+
+	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/commission"
+)
+
+func CreateGoodOrderPercent(ctx context.Context, in *npool.CommissionReq) (*npool.Commission, error) {
+	return nil, nil
+}
