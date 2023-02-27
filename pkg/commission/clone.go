aadb89f8a31a1585547bdaff5a2888d00e2951d1
JobNing <q178149@163.com>
2023-02-27 19:00:57 +0800

    update clone commission

--- pkg/commission/clone.go
+++ pkg/commission/clone.go
@@ -6,8 +6,8 @@ import (
 	gop "github.com/NpoolPlatform/inspire-middleware/pkg/commission/goodorderpercent"
 )
 
-func CloneCommissions(ctx context.Context, appID, fromGoodID, toGoodID string) error {
-	if err := gop.CloneGoodOrderPercents(ctx, appID, fromGoodID, toGoodID); err != nil {
+func CloneCommissions(ctx context.Context, appID, fromGoodID, toGoodID, value string) error {
+	if err := gop.CloneGoodOrderPercents(ctx, appID, fromGoodID, toGoodID, value); err != nil {
 		return err
 	}
 	return nil
--- pkg/commission/goodorderpercent/clone.go
+++ pkg/commission/goodorderpercent/clone.go
@@ -3,6 +3,8 @@ package goodorderpercent
 import (
 	"context"
 
+	"github.com/shopspring/decimal"
+
 	"github.com/NpoolPlatform/inspire-manager/pkg/db"
 	"github.com/NpoolPlatform/inspire-manager/pkg/db/ent"
 
@@ -11,7 +13,14 @@ import (
 	"github.com/google/uuid"
 )
 
-func CloneGoodOrderPercents(ctx context.Context, appID, fromGoodID, toGoodID string) error {
+func CloneGoodOrderPercents(ctx context.Context, appID, fromGoodID, toGoodID, value string) error {
+	val, err := decimal.NewFromString(value)
+	if err != nil {
+		return err
+	}
+	if val.Cmp(decimal.NewFromInt(0)) == 0 {
+		return nil
+	}
 	return db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
 		gops, err := cli.
 			GoodOrderPercent.
@@ -44,17 +53,22 @@ func CloneGoodOrderPercents(ctx context.Context, appID, fromGoodID, toGoodID str
 					return err
 				}
 			}
-			if _gop != nil {
+			if _gop != nil || _gop.Percent.Cmp(decimal.NewFromInt(0)) == 0 {
 				continue
 			}
-
 			_, err = cli.
 				GoodOrderPercent.
 				Create().
 				SetAppID(gop.AppID).
 				SetUserID(gop.UserID).
 				SetGoodID(uuid.MustParse(toGoodID)).
-				SetPercent(gop.Percent).
+				SetPercent(
+					gop.
+						Percent.
+						Mul(val).
+						Div(decimal.NewFromInt(100)). //nolint
+						RoundUp(0),
+				).
 				SetStartAt(gop.StartAt).
 				SetEndAt(0).
 				Save(_ctx)
