5be1dd7978600325f99bc7aabf84806ce6e635af
JobNing <q178149@163.com>
2023-03-28 17:26:13 +0800

    create sub

--- pkg/message/sub/sub.go
+++ pkg/message/sub/sub.go
@@ -1,1 +1,45 @@
 package sub
+
+import (
+	"encoding/json"
+	"fmt"
+	msgcli "github.com/NpoolPlatform/go-service-framework/pkg/rabbitmq/client"
+	constant "github.com/NpoolPlatform/inspire-middleware/pkg/message/const"
+)
+
+type SignMsg struct {
+	AppID     string `json:"app_id"`
+	InviterID string `json:"inviter_id"`
+	InviteeID string `json:"invitee_id"`
+}
+
+func Sign(h func(*SignMsg) error) error {
+	myClient, err := msgcli.New(constant.ServiceName)
+	if err != nil {
+		return err
+	}
+	err = myClient.DeclareSub(constant.ServiceName, "sign")
+	if err != nil {
+		return err
+	}
+	msgs, err := myClient.Consume(constant.ServiceName)
+	if err != nil {
+		return err
+	}
+	for d := range msgs {
+		msg := SignMsg{}
+		err := json.Unmarshal(d.Body, &msg)
+		if err != nil {
+			return fmt.Errorf("parse message example error: %v", err)
+		}
+
+		if h != nil {
+			err = h(&msg)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return fmt.Errorf("WE SHOULD NOT BE HERE")
+}
