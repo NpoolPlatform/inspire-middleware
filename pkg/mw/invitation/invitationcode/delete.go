21adae5f76630d2daa15766bbafa5595a21a2b2b
Zhao KK <kikakkz@hotmail.com>
2023-07-26 13:11:04 +0000

    Add invitationcode delete and update apis

--- pkg/mw/invitation/invitationcode/delete.go
+++ pkg/mw/invitation/invitationcode/delete.go
@@ -2,11 +2,43 @@ package invitationcode
 
 import (
 	"context"
+	"fmt"
+	"time"
 
-	mgrcli "github.com/NpoolPlatform/inspire-manager/pkg/client/invitation/invitationcode"
-	mgrpb "github.com/NpoolPlatform/message/npool/inspire/mgr/v1/invitation/invitationcode"
+	invitationcodecrud "github.com/NpoolPlatform/inspire-middleware/pkg/crud/invitation/invitationcode"
+	"github.com/NpoolPlatform/inspire-middleware/pkg/db"
+	"github.com/NpoolPlatform/inspire-middleware/pkg/db/ent"
+	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/invitation/invitationcode"
 )
 
-func DeleteInvitationCode(ctx context.Context, id string) (*mgrpb.InvitationCode, error) {
-	return mgrcli.DeleteInvitationCode(ctx, id)
+func (h *Handler) DeleteInvitationCode(ctx context.Context, in *npool.InvitationCodeReq) (*npool.InvitationCode, error) {
+	if h.ID == nil {
+		return nil, fmt.Errorf("invalid id")
+	}
+
+	info, err := h.GetInvitationCode(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if info == nil {
+		return nil, nil
+	}
+
+	now := uint32(time.Now().Unix())
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		if _, err := invitationcodecrud.UpdateSet(
+			cli.InvitationCode.UpdateOneID(*h.ID),
+			&invitationcodecrud.Req{
+				DeletedAt: &now,
+			},
+		).Save(_ctx); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
 }
--- pkg/mw/invitation/invitationcode/update.go
+++ pkg/mw/invitation/invitationcode/update.go
@@ -2,11 +2,33 @@ package invitationcode
 
 import (
 	"context"
+	"fmt"
 
-	mgrcli "github.com/NpoolPlatform/inspire-manager/pkg/client/invitation/invitationcode"
-	mgrpb "github.com/NpoolPlatform/message/npool/inspire/mgr/v1/invitation/invitationcode"
+	invitationcodecrud "github.com/NpoolPlatform/inspire-middleware/pkg/crud/invitation/invitationcode"
+	"github.com/NpoolPlatform/inspire-middleware/pkg/db"
+	"github.com/NpoolPlatform/inspire-middleware/pkg/db/ent"
+	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/invitation/invitationcode"
 )
 
-func UpdateInvitationCode(ctx context.Context, in *mgrpb.InvitationCodeReq) (*mgrpb.InvitationCode, error) {
-	return mgrcli.UpdateInvitationCode(ctx, in)
+func (h *Handler) UpdateInvitationCode(ctx context.Context, in *npool.InvitationCodeReq) (*npool.InvitationCode, error) {
+	if h.ID == nil {
+		return nil, fmt.Errorf("invalid id")
+	}
+
+	err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		if _, err := invitationcodecrud.UpdateSet(
+			cli.InvitationCode.UpdateOneID(*h.ID),
+			&invitationcodecrud.Req{
+				Disabled: h.Disabled,
+			},
+		).Save(_ctx); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return h.GetInvitationCode(ctx)
 }
