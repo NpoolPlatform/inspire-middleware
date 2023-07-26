aab6441986a692562119403843e9e3046442bf10
Zhao KK <kikakkz@hotmail.com>
2023-07-26 06:10:24 +0000

    Add coupon api

--- api/coupon/query.go
+++ api/coupon/query.go
@@ -2,58 +2,39 @@ package coupon
 
 import (
 	"context"
-	"fmt"
 
-	allocatedmgrpb "github.com/NpoolPlatform/message/npool/inspire/mgr/v1/coupon/allocated"
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+
+	coupon1 "github.com/NpoolPlatform/inspire-middleware/pkg/mw/coupon"
 	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/coupon"
-
-	coupon1 "github.com/NpoolPlatform/inspire-middleware/pkg/coupon/coupon"
-
-	constant "github.com/NpoolPlatform/inspire-middleware/pkg/const"
-
-	"github.com/google/uuid"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func ValidateConds(conds *npool.Conds) error {
-	if conds.ID != nil {
-		if _, err := uuid.Parse(conds.GetID().GetValue()); err != nil {
-			return err
-		}
-	}
-	if conds.AppID != nil {
-		if _, err := uuid.Parse(conds.GetAppID().GetValue()); err != nil {
-			return err
-		}
-	}
-	if conds.GoodID != nil {
-		if _, err := uuid.Parse(conds.GetGoodID().GetValue()); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
 func (s *Server) GetCoupons(ctx context.Context, in *npool.GetCouponsRequest) (*npool.GetCouponsResponse, error) {
-	conds := in.GetConds()
-	if conds == nil {
-		conds = &npool.Conds{}
-	}
-
-	if err := ValidateConds(in.GetConds()); err != nil {
+	handler, err := coupon1.NewHandler(
+		ctx,
+		coupon1.WithConds(in.GetConds()),
+		coupon1.WithOffset(in.GetOffset()),
+		coupon1.WithLimit(in.GetLimit()),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetCoupons",
+			"In", in,
+			"Err", err,
+		)
 		return &npool.GetCouponsResponse{}, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	limit := constant.DefaultRowLimit
-	if in.GetLimit() > 0 {
-		limit = in.GetLimit()
-	}
-
-	infos, total, err := coupon1.GetCoupons(ctx, conds, in.GetOffset(), limit)
+	infos, total, err := handler.GetCoupons(ctx)
 	if err != nil {
+		logger.Sugar().Errorw(
+			"GetCoupons",
+			"In", in,
+			"Err", err,
+		)
 		return &npool.GetCouponsResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
@@ -64,32 +45,26 @@ func (s *Server) GetCoupons(ctx context.Context, in *npool.GetCouponsRequest) (*
 }
 
 func (s *Server) GetCoupon(ctx context.Context, in *npool.GetCouponRequest) (*npool.GetCouponResponse, error) {
-	if _, err := uuid.Parse(in.GetID()); err != nil {
+	handler, err := coupon1.NewHandler(
+		ctx,
+		coupon1.WithID(&in.ID),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetCoupon",
+			"In", in,
+			"Err", err,
+		)
 		return &npool.GetCouponResponse{}, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	switch in.GetCouponType() {
-	case allocatedmgrpb.CouponType_FixAmount:
-	case allocatedmgrpb.CouponType_Discount:
-	case allocatedmgrpb.CouponType_SpecialOffer:
-	case allocatedmgrpb.CouponType_ThresholdFixAmount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_ThresholdDiscount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_GoodFixAmount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_GoodDiscount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_GoodThresholdFixAmount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_GoodThresholdDiscount:
-		return nil, fmt.Errorf("not implemented")
-	default:
-		return nil, fmt.Errorf("unknown coupon type")
-	}
-
-	info, err := coupon1.GetCoupon(ctx, in.GetID(), in.GetCouponType())
+	info, err := handler.GetCoupon(ctx)
 	if err != nil {
+		logger.Sugar().Errorw(
+			"GetCoupon",
+			"In", in,
+			"Err", err,
+		)
 		return &npool.GetCouponResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
--- api/coupon/update.go
+++ api/coupon/update.go
@@ -2,180 +2,52 @@ package coupon
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 
-	allocatedmgrpb "github.com/NpoolPlatform/message/npool/inspire/mgr/v1/coupon/allocated"
+	coupon1 "github.com/NpoolPlatform/inspire-middleware/pkg/mw/coupon"
 	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/coupon"
-
-	coupon1 "github.com/NpoolPlatform/inspire-middleware/pkg/coupon/coupon"
-
-	"github.com/google/uuid"
-	"github.com/shopspring/decimal"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func ValidateUpdate(ctx context.Context, info *npool.CouponReq) error { //nolint
-	if _, err := uuid.Parse(info.GetID()); err != nil {
-		return err
-	}
-
-	switch info.GetCouponType() {
-	case allocatedmgrpb.CouponType_FixAmount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_SpecialOffer:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_ThresholdFixAmount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_ThresholdDiscount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_GoodFixAmount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_GoodThresholdFixAmount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_Discount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_GoodDiscount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_GoodThresholdDiscount:
-	default:
-		return fmt.Errorf("invalid coupon type")
-	}
-
-	coup, err := coupon1.GetCoupon(ctx, info.GetID(), info.GetCouponType())
-	if err != nil {
-		return err
-	}
-	if coup == nil {
-		return fmt.Errorf("invalid coupon")
-	}
-
-	if coup.CouponType != allocatedmgrpb.CouponType_SpecialOffer {
-		allocated, err := decimal.NewFromString(coup.Allocated)
-		if err != nil {
-			return err
-		}
-		if allocated.Cmp(decimal.NewFromInt(0)) > 0 {
-			if info.Value != nil || info.Circulation != nil {
-				return fmt.Errorf("permission denied")
-			}
-		}
-	}
-
-	value, err := decimal.NewFromString(coup.Value)
-	if err != nil {
-		return err
-	}
-
-	circulation, err := decimal.NewFromString(coup.Circulation)
-	if err != nil {
-		return err
-	}
-
-	if info.Value != nil {
-		value, err = decimal.NewFromString(info.GetValue())
-		if err != nil {
-			return err
-		}
-	}
-
-	if info.Circulation != nil {
-		circulation, err = decimal.NewFromString(info.GetCirculation())
-		if err != nil {
-			return err
-		}
-	}
-
-	switch info.GetCouponType() {
-	case allocatedmgrpb.CouponType_FixAmount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_ThresholdFixAmount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_ThresholdDiscount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_GoodFixAmount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_GoodThresholdFixAmount:
-		if info.Value != nil || info.Circulation != nil {
-			if circulation.Cmp(value) < 0 {
-				return fmt.Errorf("value overflow")
-			}
-		}
-	case allocatedmgrpb.CouponType_Discount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_GoodDiscount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_GoodThresholdDiscount:
-		if info.Value != nil {
-			if value.Cmp(decimal.NewFromInt(100)) >= 0 { //nolint
-				return fmt.Errorf("value overflow")
-			}
-		}
-		if info.Circulation != nil {
-			if circulation.Cmp(decimal.NewFromInt(0)) <= 0 {
-				return fmt.Errorf("invalid circulation")
-			}
-		}
-	default:
-	}
-
-	if info.DurationDays != nil && info.GetDurationDays() <= 0 {
-		return fmt.Errorf("invalid durationdays")
-	}
-
-	if info.Message != nil && info.GetMessage() == "" {
-		return fmt.Errorf("invalid message")
-	}
-	if info.Name != nil && info.GetName() == "" && info.GetCouponType() != allocatedmgrpb.CouponType_SpecialOffer {
-		return fmt.Errorf("invalid name")
-	}
-
-	if info.GetCouponType() == allocatedmgrpb.CouponType_SpecialOffer {
-		if info.UserID != nil {
-			if _, err := uuid.Parse(info.GetUserID()); err != nil {
-				return err
-			}
-		}
-	}
-
-	switch info.GetCouponType() {
-	case allocatedmgrpb.CouponType_GoodFixAmount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_GoodDiscount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_GoodThresholdFixAmount:
-		fallthrough //nolint
-	case allocatedmgrpb.CouponType_GoodThresholdDiscount:
-		if info.GoodID != nil {
-			if _, err := uuid.Parse(info.GetGoodID()); err != nil {
-				return err
-			}
-		}
-	}
-
-	if info.Threshold != nil {
-		if _, err := decimal.NewFromString(info.GetThreshold()); err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
 func (s *Server) UpdateCoupon(ctx context.Context, in *npool.UpdateCouponRequest) (*npool.UpdateCouponResponse, error) {
-	if err := ValidateUpdate(ctx, in.GetInfo()); err != nil {
-		fmt.Println("******************err")
-		fmt.Println(err)
-		logger.Sugar().Errorw("UpdateCoupon", "error", err)
+	req := in.GetInfo()
+	if req == nil {
+		logger.Sugar().Errorw(
+			"UpdateCoupon",
+			"In", in,
+		)
+		return &npool.UpdateCouponResponse{}, status.Error(codes.InvalidArgument, "invalid info")
+	}
+	handler, err := coupon1.NewHandler(
+		ctx,
+		coupon1.WithID(req.ID),
+		coupon1.WithDenomination(req.Denomination),
+		coupon1.WithCirculation(req.Circulation),
+		coupon1.WithStartAt(req.StartAt),
+		coupon1.WithDurationDays(req.DurationDays),
+		coupon1.WithMessage(req.Message),
+		coupon1.WithName(req.Name),
+		coupon1.WithAllocated(req.Allocated),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"UpdateCoupon",
+			"In", in,
+			"Err", err,
+		)
 		return &npool.UpdateCouponResponse{}, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	info, err := coupon1.UpdateCoupon(ctx, in.GetInfo())
+	info, err := handler.UpdateCoupon(ctx)
 	if err != nil {
-		logger.Sugar().Errorw("UpdateCoupon", "error", err)
+		logger.Sugar().Errorw(
+			"UpdateCoupon",
+			"In", in,
+			"Err", err,
+		)
 		return &npool.UpdateCouponResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
