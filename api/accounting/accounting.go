7e2fc3ef9750e4336214ea95cf40b7702480a286
Zhao KK <kikakkz@hotmail.com>
2023-07-28 04:14:35 +0000

    Add accounting API

--- api/accounting/accounting.go
+++ api/accounting/accounting.go
@@ -3,85 +3,50 @@ package accounting
 import (
 	"context"
 
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+
+	accounting1 "github.com/NpoolPlatform/inspire-middleware/pkg/mw/accounting"
 	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/accounting"
-
-	accounting1 "github.com/NpoolPlatform/inspire-middleware/pkg/accounting"
-
-	"github.com/google/uuid"
-	"github.com/shopspring/decimal"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-//nolint:gocyclo
 func (s *Server) Accounting(ctx context.Context, in *npool.AccountingRequest) (*npool.AccountingResponse, error) {
-	if _, err := uuid.Parse(in.GetAppID()); err != nil {
-		return &npool.AccountingResponse{}, status.Error(codes.InvalidArgument, err.Error())
-	}
-	if _, err := uuid.Parse(in.GetUserID()); err != nil {
-		return &npool.AccountingResponse{}, status.Error(codes.InvalidArgument, err.Error())
-	}
-	if _, err := uuid.Parse(in.GetGoodID()); err != nil {
-		return &npool.AccountingResponse{}, status.Error(codes.InvalidArgument, err.Error())
-	}
-	if _, err := uuid.Parse(in.GetOrderID()); err != nil {
-		return &npool.AccountingResponse{}, status.Error(codes.InvalidArgument, err.Error())
-	}
-	if _, err := uuid.Parse(in.GetPaymentID()); err != nil {
-		return &npool.AccountingResponse{}, status.Error(codes.InvalidArgument, err.Error())
-	}
-	if _, err := uuid.Parse(in.GetCoinTypeID()); err != nil {
-		return &npool.AccountingResponse{}, status.Error(codes.InvalidArgument, err.Error())
-	}
-	if _, err := uuid.Parse(in.GetPaymentCoinTypeID()); err != nil {
-		return &npool.AccountingResponse{}, status.Error(codes.InvalidArgument, err.Error())
-	}
-	paymentCoinUSDCurrency, err := decimal.NewFromString(in.GetPaymentCoinUSDCurrency())
-	if err != nil {
-		return &npool.AccountingResponse{}, status.Error(codes.InvalidArgument, err.Error())
-	}
-	if paymentCoinUSDCurrency.Cmp(decimal.NewFromInt(0)) <= 0 {
-		return &npool.AccountingResponse{}, status.Error(codes.InvalidArgument, "PaymentCoinUSDCurrency <= 0")
-	}
-	untis, err := decimal.NewFromString(in.GetUnits())
-	if err != nil {
-		return &npool.AccountingResponse{}, status.Error(codes.InvalidArgument, "Units == 0")
-	}
-	if untis.Cmp(decimal.NewFromInt(0)) == 0 {
-		return &npool.AccountingResponse{}, status.Error(codes.InvalidArgument, "Units == 0")
-	}
-
-	paymentAmount, err := decimal.NewFromString(in.GetPaymentAmount())
-	if err != nil {
-		return &npool.AccountingResponse{}, status.Error(codes.InvalidArgument, err.Error())
-	}
-	goodValue, err := decimal.NewFromString(in.GetGoodValue())
-	if err != nil {
-		return &npool.AccountingResponse{}, status.Error(codes.InvalidArgument, err.Error())
-	}
-	if goodValue.Cmp(decimal.NewFromInt(0)) <= 0 {
-		return &npool.AccountingResponse{}, status.Error(codes.InvalidArgument, "GoodValue is invalid")
-	}
-
-	infos, err := accounting1.Accounting(
+	handler, err := accounting1.NewHandler(
 		ctx,
-		in.GetAppID(),
-		in.GetUserID(),
-		in.GetGoodID(),
-		in.GetOrderID(),
-		in.GetPaymentID(),
-		in.GetCoinTypeID(),
-		in.GetPaymentCoinTypeID(),
-		paymentCoinUSDCurrency,
-		in.GetUnits(),
-		in.GetSettleType(),
-		paymentAmount,
-		goodValue,
-		in.GetHasCommission(),
-		in.GetOrderCreatedAt(),
+		accounting1.WithAppID(in.GetAppID()),
+		accounting1.WithUserID(in.GetUserID()),
+		accounting1.WithGoodID(in.GetGoodID()),
+		accounting1.WithOrderID(in.GetOrderID()),
+		accounting1.WithPaymentID(in.GetPaymentID()),
+		accounting1.WithCoinTypeID(in.GetCoinTypeID()),
+		accounting1.WithPaymentCoinTypeID(in.GetPaymentCoinTypeID()),
+		accounting1.WithPaymentCoinUSDCurrency(in.GetPaymentCoinUSDCurrency()),
+		accounting1.WithUnits(in.GetUnits()),
+		accounting1.WithSettleType(in.GetSettleType()),
+		accounting1.WithSettleMode(in.GetSettleMode()),
+		accounting1.WithPaymentAmount(in.GetPaymentAmount()),
+		accounting1.WithGoodValue(in.GetGoodValue()),
+		accounting1.WithHasCommission(in.GetHasCommission()),
+		accounting1.WithOrderCreatedAt(in.GetOrderCreatedAt()),
 	)
 	if err != nil {
+		logger.Sugar().Errorw(
+			"Accounting",
+			"In", in,
+			"Err", err,
+		)
+		return &npool.AccountingResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	infos, err := handler.Accounting(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"Accounting",
+			"In", in,
+			"Err", err,
+		)
 		return &npool.AccountingResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
