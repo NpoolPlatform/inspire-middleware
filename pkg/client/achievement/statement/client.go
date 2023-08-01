8ac07b0106dfdb6006ebb6061bd05c9aba1e8a2c
Zhao KK <kikakkz@hotmail.com>
2023-08-01 10:51:35 +0000

    Add statement apis

--- pkg/client/achievement/statement/client.go
+++ pkg/client/achievement/statement/client.go
@@ -32,6 +32,38 @@ func do(ctx context.Context, handler handler) (cruder.Any, error) {
 	return handler(_ctx, cli)
 }
 
+func CreateStatement(ctx context.Context, req *npool.StatementReq) (*npool.Statement, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.CreateStatement(ctx, &npool.CreateStatementRequest{
+			Info: req,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.Statement), nil
+}
+
+func CreateStatements(ctx context.Context, reqs []*npool.StatementReq) ([]*npool.Statement, error) {
+	infos, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.CreateStatements(ctx, &npool.CreateStatementsRequest{
+			Infos: reqs,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Infos, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return infos.([]*npool.Statement), nil
+}
+
 func GetStatements(ctx context.Context, conds *npool.Conds, offset, limit int32) ([]*npool.Statement, uint32, error) {
 	var total uint32
 	infos, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
@@ -50,4 +82,22 @@ func GetStatements(ctx context.Context, conds *npool.Conds, offset, limit int32)
 		return nil, 0, err
 	}
 	return infos.([]*npool.Statement), total, nil
+}
+
+func DeleteStatement(ctx context.Context, id string) (*npool.Statement, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.DeleteStatement(ctx, &npool.DeleteStatementRequest{
+			Info: &npool.StatementReq{
+				ID: &id,
+			},
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.Statement), nil
 }
