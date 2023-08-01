4c595faff2d8ce8651e2a589224340e93aa246d6
Zhao KK <kikakkz@hotmail.com>
2023-08-01 09:32:40 +0000

    Add event client api

--- pkg/client/event/client.go
+++ pkg/client/event/client.go
@@ -17,7 +17,7 @@ var timeout = 10 * time.Second
 
 type handler func(context.Context, npool.MiddlewareClient) (cruder.Any, error)
 
-func withCRUD(ctx context.Context, handler handler) (cruder.Any, error) {
+func do(ctx context.Context, handler handler) (cruder.Any, error) {
 	_ctx, cancel := context.WithTimeout(ctx, timeout)
 	defer cancel()
 
@@ -33,8 +33,82 @@ func withCRUD(ctx context.Context, handler handler) (cruder.Any, error) {
 	return handler(_ctx, cli)
 }
 
+func CreateEvent(ctx context.Context, req *npool.EventReq) (*npool.Event, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.CreateEvent(ctx, &npool.CreateEventRequest{
+			Info: req,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.Event), nil
+}
+
+func UpdateEvent(ctx context.Context, req *npool.EventReq) (*npool.Event, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.UpdateEvent(ctx, &npool.UpdateEventRequest{
+			Info: req,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.Event), nil
+}
+
+func GetEvent(ctx context.Context, id string) (*npool.Event, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetEvent(ctx, &npool.GetEventRequest{
+			ID: id,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.Event), nil
+}
+
+func GetEvents(ctx context.Context, conds *npool.Conds, offset, limit int32) ([]*npool.Event, uint32, error) {
+	total := uint32(0)
+
+	infos, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetEvents(ctx, &npool.GetEventsRequest{
+			Conds:  conds,
+			Offset: offset,
+			Limit:  limit,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		total = resp.Total
+
+		return resp.Infos, nil
+	})
+	if err != nil {
+		return nil, 0, err
+	}
+	return infos.([]*npool.Event), total, nil
+}
+
 func GetEventOnly(ctx context.Context, conds *npool.Conds) (*npool.Event, error) {
-	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+	infos, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
 		resp, err := cli.GetEvents(ctx, &npool.GetEventsRequest{
 			Conds: conds,
 		})
@@ -56,8 +130,27 @@ func GetEventOnly(ctx context.Context, conds *npool.Conds) (*npool.Event, error)
 	return infos.([]*npool.Event)[0], nil
 }
 
+func DeleteEvent(ctx context.Context, id string) (*npool.Event, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.DeleteEvent(ctx, &npool.DeleteEventRequest{
+			Info: &npool.EventReq{
+				ID: &id,
+			},
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.Event), nil
+}
+
 func RewardEvent(ctx context.Context, req *npool.RewardEventRequest) ([]*npool.Credit, error) {
-	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+	infos, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
 		resp, err := cli.RewardEvent(ctx, req)
 		if err != nil {
 			return nil, err
