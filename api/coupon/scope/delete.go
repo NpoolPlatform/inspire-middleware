8fe33e7cc0a3132b4794ffaf40341086f199c9f4
lidaqiang <dqli161@163.com>
2023-10-10 17:05:05 +0800

    add exist & query

--- api/coupon/scope/delete.go
+++ api/coupon/scope/delete.go
@@ -2,6 +2,7 @@ package scope
 
 import (
 	"context"
+
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 	scope1 "github.com/NpoolPlatform/inspire-middleware/pkg/mw/coupon/scope"
 	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/coupon/scope"
@@ -9,43 +10,39 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func (s *Server) DeleteScope(ctx context.Context, in *npool.CreateScopeRequest) (*npool.CreateScopeResponse, error) {
+func (s *Server) DeleteScope(ctx context.Context, in *npool.DeleteScopeRequest) (*npool.DeleteScopeResponse, error) {
 	req := in.GetInfo()
 	if req == nil {
 		logger.Sugar().Errorw(
-			"CreateScope",
+			"DeleteScope",
 			"In", in,
 		)
-		return &npool.CreateScopeResponse{}, status.Error(codes.Aborted, "invalid info")
+		return &npool.DeleteScopeResponse{}, status.Error(codes.Aborted, "invalid info")
 	}
 
 	handler, err := scope1.NewHandler(
 		ctx,
 		scope1.WithID(req.ID, false),
-		scope1.WithAppID(req.AppID, true),
-		scope1.WithAppGoodID(req.AppGoodID, true),
-		scope1.WithCouponID(req.CouponID, true),
-		scope1.WithCouponScope(req.CouponScope, true),
 	)
 	if err != nil {
 		logger.Sugar().Errorw(
-			"CreateScope",
+			"DeleteScope",
 			"In", in,
 			"Err", err,
 		)
-		return &npool.CreateScopeResponse{}, status.Error(codes.Aborted, err.Error())
+		return &npool.DeleteScopeResponse{}, status.Error(codes.Aborted, err.Error())
 	}
 
-	info, err := handler.CreateScope(ctx)
+	info, err := handler.DeleteScope(ctx)
 	if err != nil {
 		logger.Sugar().Errorw(
-			"CreateScope",
+			"DeleteScope",
 			"In", in,
 			"Err", err,
 		)
-		return &npool.CreateScopeResponse{}, status.Error(codes.Aborted, err.Error())
+		return &npool.DeleteScopeResponse{}, status.Error(codes.Aborted, err.Error())
 	}
-	return &npool.CreateScopeResponse{
+	return &npool.DeleteScopeResponse{
 		Info: info,
 	}, nil
 }
---
+++ api/coupon/scope/query.go
@@ -0,0 +1,74 @@
+package scope
+
+import (
+	"context"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+
+	scope1 "github.com/NpoolPlatform/inspire-middleware/pkg/mw/coupon/scope"
+	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/coupon/scope"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) GetScope(ctx context.Context, in *npool.GetScopeRequest) (*npool.GetScopeResponse, error) {
+	handler, err := scope1.NewHandler(
+		ctx,
+		scope1.WithID(&in.ID, true),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetScope",
+			"In", in,
+			"Err", err,
+		)
+		return &npool.GetScopeResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	info, err := handler.GetScope(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetScope",
+			"In", in,
+			"Err", err,
+		)
+		return &npool.GetScopeResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &npool.GetScopeResponse{
+		Info: info,
+	}, nil
+}
+
+func (s *Server) GetScopes(ctx context.Context, in *npool.GetScopesRequest) (*npool.GetScopesResponse, error) {
+	handler, err := scope1.NewHandler(
+		ctx,
+		scope1.WithConds(in.GetConds()),
+		scope1.WithOffset(in.GetOffset()),
+		scope1.WithLimit(in.GetLimit()),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetScopes",
+			"In", in,
+			"Err", err,
+		)
+		return &npool.GetScopesResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	infos, total, err := handler.GetScopes(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"GetScopes",
+			"In", in,
+			"Err", err,
+		)
+		return &npool.GetScopesResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &npool.GetScopesResponse{
+		Infos: infos,
+		Total: total,
+	}, nil
+}
