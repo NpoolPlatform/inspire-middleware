6382169c0002ede68cd71b260227cb6a15c9dbff
lidaqiang <dqli161@163.com>
2023-10-10 12:15:22 +0800

    add scope handler

---
+++ pkg/mw/coupon/scope/handler.go
@@ -0,0 +1,208 @@
+package scope
+
+import (
+	"context"
+	"fmt"
+
+	constant "github.com/NpoolPlatform/inspire-middleware/pkg/const"
+	scopecrud "github.com/NpoolPlatform/inspire-middleware/pkg/crud/coupon/scope"
+	coupon1 "github.com/NpoolPlatform/inspire-middleware/pkg/mw/coupon"
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	types "github.com/NpoolPlatform/message/npool/basetypes/inspire/v1"
+	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/coupon/scope"
+
+	"github.com/google/uuid"
+)
+
+type Handler struct {
+	ID          *uuid.UUID
+	AppID       *uuid.UUID
+	AppGoodID   *uuid.UUID
+	CouponID    *uuid.UUID
+	CouponScope *types.CouponScope
+	Conds       *scopecrud.Conds
+	Offset      int32
+	Limit       int32
+}
+
+func NewHandler(ctx context.Context, options ...func(context.Context, *Handler) error) (*Handler, error) {
+	handler := &Handler{}
+	for _, opt := range options {
+		if err := opt(ctx, handler); err != nil {
+			return nil, err
+		}
+	}
+	return handler, nil
+}
+
+func WithID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid id")
+			}
+			return nil
+		}
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.ID = &_id
+		return nil
+	}
+}
+
+func WithAppID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid appid")
+			}
+			return nil
+		}
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.AppID = &_id
+		return nil
+	}
+}
+
+func WithAppGoodID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid userid")
+			}
+			return nil
+		}
+		_id, err := uuid.Parse(*id)
+		if err != nil {
+			return err
+		}
+		h.AppGoodID = &_id
+		return nil
+	}
+}
+
+func WithCouponID(id *string, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			if must {
+				return fmt.Errorf("invalid couponid")
+			}
+			return nil
+		}
+		handler, err := coupon1.NewHandler(
+			ctx,
+			coupon1.WithID(id, true),
+		)
+		if err != nil {
+			return err
+		}
+		exist, err := handler.ExistCoupon(ctx)
+		if err != nil {
+			return err
+		}
+		if !exist {
+			return fmt.Errorf("invalid couponid")
+		}
+		_id := uuid.MustParse(*id)
+		h.CouponID = &_id
+		return nil
+	}
+}
+
+func WithCouponScope(couponScope *types.CouponScope, must bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if couponScope == nil {
+			if must {
+				return fmt.Errorf("invalid couponscope")
+			}
+			return nil
+		}
+		switch *couponScope {
+		case types.CouponScope_AllGood:
+		case types.CouponScope_Blacklist:
+		case types.CouponScope_Whitelist:
+		default:
+			return fmt.Errorf("invalid couponscope")
+		}
+		h.CouponScope = couponScope
+		return nil
+	}
+}
+
+func WithConds(conds *npool.Conds) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.Conds = &scopecrud.Conds{}
+		if conds == nil {
+			return nil
+		}
+		if conds.ID != nil {
+			id, err := uuid.Parse(conds.GetID().GetValue())
+			if err != nil {
+				return err
+			}
+			h.Conds.ID = &cruder.Cond{
+				Op:  conds.GetID().GetOp(),
+				Val: id,
+			}
+		}
+		if conds.AppID != nil {
+			id, err := uuid.Parse(conds.GetAppID().GetValue())
+			if err != nil {
+				return err
+			}
+			h.Conds.AppID = &cruder.Cond{
+				Op:  conds.GetAppID().GetOp(),
+				Val: id,
+			}
+		}
+		if conds.AppGoodID != nil {
+			id, err := uuid.Parse(conds.GetAppGoodID().GetValue())
+			if err != nil {
+				return err
+			}
+			h.Conds.AppGoodID = &cruder.Cond{
+				Op:  conds.GetAppGoodID().GetOp(),
+				Val: id,
+			}
+		}
+		if conds.CouponID != nil {
+			id, err := uuid.Parse(conds.GetCouponID().GetValue())
+			if err != nil {
+				return err
+			}
+			h.Conds.CouponID = &cruder.Cond{
+				Op:  conds.GetCouponID().GetOp(),
+				Val: id,
+			}
+		}
+		if conds.CouponScope != nil {
+			h.Conds.CouponScope = &cruder.Cond{
+				Op:  conds.GetCouponScope().GetOp(),
+				Val: types.CouponScope(conds.GetCouponScope().GetValue()),
+			}
+		}
+		return nil
+	}
+}
+
+func WithOffset(value int32) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.Offset = value
+		return nil
+	}
+}
+
+func WithLimit(value int32) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if value == 0 {
+			value = constant.DefaultRowLimit
+		}
+		h.Limit = value
+		return nil
+	}
+}
