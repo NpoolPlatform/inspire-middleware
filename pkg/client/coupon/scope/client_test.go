37769ec21a18f1c28e2ac5fb354cfa43debbef92
lidaqiang <dqli161@163.com>
2023-10-10 18:56:24 +0800

    add client

--- pkg/client/coupon/scope/client_test.go
+++ pkg/client/coupon/scope/client_test.go
@@ -9,17 +9,19 @@ import (
 	"time"
 
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
-	"github.com/shopspring/decimal"
 
-	coupon1 "github.com/NpoolPlatform/inspire-middleware/pkg/mw/coupon"
+	couponmwcli "github.com/NpoolPlatform/inspire-middleware/pkg/client/coupon"
 	couponmwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/coupon"
 	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/coupon/scope"
-	"github.com/google/uuid"
-	"github.com/stretchr/testify/assert"
 
-	"github.com/NpoolPlatform/inspire-middleware/pkg/testinit"
 	types "github.com/NpoolPlatform/message/npool/basetypes/inspire/v1"
 	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+
+	"github.com/NpoolPlatform/inspire-middleware/pkg/testinit"
+
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/assert"
 )
 
 func init() {
@@ -47,8 +49,6 @@ var (
 		CouponConstraint:    types.CouponConstraint_Normal,
 		CouponConstraintStr: types.CouponConstraint_Normal.String(),
 		Allocated:           "0",
-		CouponScope:         types.CouponScope_Whitelist,
-		CouponScopeStr:      types.CouponScope_Whitelist.String(),
 	}
 
 	ret = npool.Scope{
@@ -64,46 +64,33 @@ var (
 )
 
 func setup(t *testing.T) func(*testing.T) {
-	h1, err := coupon1.NewHandler(
-		context.Background(),
-		coupon1.WithID(&coupon.ID, true),
-		coupon1.WithAppID(&coupon.AppID, true),
-		coupon1.WithName(&ret.CouponName, true),
-		coupon1.WithMessage(&coupon.Message, true),
-		coupon1.WithCouponType(&coupon.CouponType, true),
-		coupon1.WithDenomination(&coupon.Denomination, true),
-		coupon1.WithCouponScope(&coupon.CouponScope, true),
-		coupon1.WithCirculation(&coupon.Circulation, true),
-		coupon1.WithDurationDays(&coupon.DurationDays, true),
-		coupon1.WithIssuedBy(&coupon.IssuedBy, true),
-		coupon1.WithStartAt(&coupon.StartAt, true),
-	)
-	assert.Nil(t, err)
-
-	coup, err := h1.CreateCoupon(context.Background())
+	info, err := couponmwcli.CreateCoupon(context.Background(), &couponmwpb.CouponReq{
+		ID:           &coupon.ID,
+		AppID:        &coupon.AppID,
+		Name:         &coupon.Name,
+		Message:      &coupon.Message,
+		CouponType:   &coupon.CouponType,
+		Denomination: &coupon.Denomination,
+		Circulation:  &coupon.Circulation,
+		DurationDays: &coupon.DurationDays,
+		IssuedBy:     &coupon.IssuedBy,
+		StartAt:      &coupon.StartAt,
+	})
 	if assert.Nil(t, err) {
-		coupon.CreatedAt = coup.CreatedAt
-		coupon.UpdatedAt = coup.UpdatedAt
-		assert.Equal(t, &coupon, coup)
+		assert.Equal(t, &coupon, info)
 	}
 
-	return func(*testing.T) {
-		_, _ = h1.DeleteCoupon(context.Background())
-	}
+	return func(*testing.T) {}
 }
 
 func createScope(t *testing.T) {
-	handler, err := NewHandler(
-		context.Background(),
-		WithID(&ret.ID, true),
-		WithAppID(&ret.AppID, true),
-		WithAppGoodID(&ret.AppGoodID, true),
-		WithCouponID(&ret.CouponID, true),
-		WithCouponScope(&ret.CouponScope, true),
-	)
-	assert.Nil(t, err)
-
-	info, err := handler.CreateScope(context.Background())
+	info, err := CreateScope(context.Background(), &npool.ScopeReq{
+		ID:          &ret.ID,
+		AppID:       &ret.AppID,
+		AppGoodID:   &ret.AppGoodID,
+		CouponID:    &ret.CouponID,
+		CouponScope: &ret.CouponScope,
+	})
 	if assert.Nil(t, err) {
 		ret.CreatedAt = info.CreatedAt
 		ret.UpdatedAt = info.UpdatedAt
@@ -112,37 +99,22 @@ func createScope(t *testing.T) {
 }
 
 func getScope(t *testing.T) {
-	handler, err := NewHandler(
-		context.Background(),
-		WithID(&ret.ID, true),
-	)
-	assert.Nil(t, err)
-
-	info, err := handler.GetScope(context.Background())
+	info, err := GetScope(context.Background(), ret.ID)
 	if assert.Nil(t, err) {
-		assert.Equal(t, info, &ret)
+		assert.Equal(t, &ret, info)
 	}
 }
 
 func getScopes(t *testing.T) {
-	conds := &npool.Conds{
+	infos, total, err := GetScopes(context.Background(), &npool.Conds{
 		ID:          &basetypes.StringVal{Op: cruder.EQ, Value: ret.ID},
 		AppID:       &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppID},
 		AppGoodID:   &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppGoodID},
 		CouponID:    &basetypes.StringVal{Op: cruder.EQ, Value: ret.CouponID},
 		CouponScope: &basetypes.Uint32Val{Op: cruder.EQ, Value: uint32(ret.CouponScope)},
-	}
-
-	handler, err := NewHandler(
-		context.Background(),
-		WithConds(conds),
-		WithOffset(0),
-		WithLimit(1),
-	)
-	assert.Nil(t, err)
-
-	infos, _, err := handler.GetScopes(context.Background())
+	}, 0, 1)
 	if assert.Nil(t, err) {
+		assert.Equal(t, total, uint32(1))
 		assert.Equal(t, len(infos), 1)
 		assert.Equal(t, &ret, infos[0])
 	}
@@ -155,30 +127,19 @@ func existScopeConds(t *testing.T) {
 		AppGoodID:   &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppGoodID},
 		CouponScope: &basetypes.Uint32Val{Op: cruder.EQ, Value: uint32(ret.CouponScope)},
 	}
-	handler, err := NewHandler(
-		context.Background(),
-		WithConds(conds),
-	)
-	assert.Nil(t, err)
 
-	exist, err := handler.ExistScopeConds(context.Background())
+	exist, err := ExistScopeConds(context.Background(), conds)
 	assert.Nil(t, err)
 	assert.True(t, exist)
 }
 
 func deleteScope(t *testing.T) {
-	handler, err := NewHandler(
-		context.Background(),
-		WithID(&ret.ID, true),
-	)
-	assert.Nil(t, err)
-
-	info, err := handler.DeleteScope(context.Background())
+	info, err := DeleteScope(context.Background(), ret.ID)
 	if assert.Nil(t, err) {
 		assert.Equal(t, info, &ret)
 	}
 
-	info, err = handler.GetScope(context.Background())
+	info, err = GetScope(context.Background(), ret.ID)
 	assert.Nil(t, err)
 	assert.Nil(t, info)
 }
@@ -194,6 +155,6 @@ func TestScope(t *testing.T) {
 	t.Run("createScope", createScope)
 	t.Run("getScope", getScope)
 	t.Run("getScopes", getScopes)
-	t.Run("existScope", existScopeConds)
+	t.Run("existScopeConds", existScopeConds)
 	t.Run("deleteScope", deleteScope)
 }
