5b987982269870d6fcd6c05ce2cfecb554cdc07f
lidaqiang <dqli161@163.com>
2023-10-11 03:39:50 +0000

    refactor create

--- pkg/mw/coupon/scope/create.go
+++ pkg/mw/coupon/scope/create.go
@@ -7,8 +7,10 @@ import (
 	scopecrud "github.com/NpoolPlatform/inspire-middleware/pkg/crud/coupon/scope"
 	"github.com/NpoolPlatform/inspire-middleware/pkg/db"
 	"github.com/NpoolPlatform/inspire-middleware/pkg/db/ent"
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/coupon/scope"
 
+	types "github.com/NpoolPlatform/message/npool/basetypes/inspire/v1"
 	"github.com/google/uuid"
 )
 
@@ -50,16 +52,41 @@ func (h *createHandler) createScope(ctx context.Context, tx *ent.Tx) error {
 }
 
 func (h *Handler) CreateScope(ctx context.Context) (*npool.Scope, error) {
-	id := uuid.New()
-	if h.ID == nil {
-		h.ID = &id
+	if h.AppGoodID == nil {
+		switch *h.CouponScope {
+		case types.CouponScope_Blacklist:
+			fallthrough //nolint
+		case types.CouponScope_Whitelist:
+			return nil, fmt.Errorf("appgoodid is must")
+		}
+	}
+	if *h.CouponScope == types.CouponScope_AllGood {
+		h.AppGoodID = &uuid.Nil
+	}
+
+	h.Conds = &scopecrud.Conds{
+		AppID:       &cruder.Cond{Op: cruder.EQ, Val: *h.AppID},
+		AppGoodID:   &cruder.Cond{Op: cruder.EQ, Val: *h.AppGoodID},
+		CouponID:    &cruder.Cond{Op: cruder.EQ, Val: *h.CouponID},
+		CouponScope: &cruder.Cond{Op: cruder.EQ, Val: *h.CouponScope},
+	}
+	exist, err := h.ExistScopeConds(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if exist {
+		return nil, fmt.Errorf("coupon scope %v already exist", *h.CouponScope)
 	}
 
 	handler := &createHandler{
 		Handler: h,
 	}
+	id := uuid.New()
+	if h.ID == nil {
+		h.ID = &id
+	}
 
-	err := db.WithTx(ctx, func(_ctx context.Context, tx *ent.Tx) error {
+	err = db.WithTx(ctx, func(_ctx context.Context, tx *ent.Tx) error {
 		if err := handler.verifyCoupon(ctx, tx); err != nil {
 			return err
 		}
