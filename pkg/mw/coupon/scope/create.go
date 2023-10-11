a0bb86dbef72d5df6aee4d9f2c2326a3ec38e6fe
lidaqiang <dqli161@163.com>
2023-10-11 02:31:19 +0000

    optimize create

--- pkg/mw/coupon/scope/create.go
+++ pkg/mw/coupon/scope/create.go
@@ -12,33 +12,58 @@ import (
 	"github.com/google/uuid"
 )
 
+type createHandler struct {
+	*Handler
+}
+
+func (h *createHandler) verifyCoupon(ctx context.Context, tx *ent.Tx) error {
+	coupon, err := tx.Coupon.Get(ctx, *h.CouponID)
+	if err != nil {
+		return err
+	}
+	if coupon == nil {
+		return fmt.Errorf("coupon not found %v", *h.CouponID)
+	}
+	if h.CouponScope.String() != coupon.CouponScope {
+		return fmt.Errorf("coupon scope not matched %v -> %v", h.CouponScope, coupon.CouponScope)
+	}
+	if *h.AppID != coupon.AppID {
+		return fmt.Errorf("appid not matched %v -> %v", *h.AppID, coupon.AppID)
+	}
+	return nil
+}
+
+func (h *createHandler) createScope(ctx context.Context, tx *ent.Tx) error {
+	if _, err := scopecrud.CreateSet(
+		tx.CouponScope.Create(),
+		&scopecrud.Req{
+			ID:          h.ID,
+			AppID:       h.AppID,
+			AppGoodID:   h.AppGoodID,
+			CouponID:    h.CouponID,
+			CouponScope: h.CouponScope,
+		},
+	).Save(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
 func (h *Handler) CreateScope(ctx context.Context) (*npool.Scope, error) {
 	id := uuid.New()
 	if h.ID == nil {
 		h.ID = &id
 	}
-	err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		coupon, err := cli.Coupon.Get(ctx, *h.CouponID)
-		if err != nil {
+
+	handler := &createHandler{
+		Handler: h,
+	}
+
+	err := db.WithTx(ctx, func(_ctx context.Context, tx *ent.Tx) error {
+		if err := handler.verifyCoupon(ctx, tx); err != nil {
 			return err
 		}
-		if coupon == nil {
-			return fmt.Errorf("coupon not found %v", *h.CouponID)
-		}
-		if h.CouponScope.String() != coupon.CouponScope {
-			return fmt.Errorf("coupon scope not matched %v -> %v", h.CouponScope, coupon.CouponScope)
-		}
-
-		if _, err := scopecrud.CreateSet(
-			cli.CouponScope.Create(),
-			&scopecrud.Req{
-				ID:          h.ID,
-				AppID:       h.AppID,
-				AppGoodID:   h.AppGoodID,
-				CouponID:    h.CouponID,
-				CouponScope: h.CouponScope,
-			},
-		).Save(_ctx); err != nil {
+		if err := handler.createScope(ctx, tx); err != nil {
 			return err
 		}
 		return nil
