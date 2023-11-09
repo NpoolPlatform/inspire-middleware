6717a20637f5ebf99481b28e9f5fbafe45928a37
lidaqiang <dqli161@163.com>
2023-11-09 15:26:12 +0800

    optimize create

--- pkg/mw/coupon/scope/create.go
+++ pkg/mw/coupon/scope/create.go
@@ -17,15 +17,17 @@ type createHandler struct {
 	*Handler
 }
 
-func (h *createHandler) verifyCoupon(ctx context.Context, cli *ent.Client) error {
-	coupon, err := cli.Coupon.Get(ctx, *h.CouponID)
-	if err != nil {
-		return err
-	}
-	if coupon == nil {
-		return fmt.Errorf("coupon not found %v", *h.CouponID)
-	}
-	return nil
+func (h *createHandler) getCoupon(ctx context.Context) error {
+	return db.WithClient(ctx, func(ctx context.Context, cli *ent.Client) error {
+		coupon, err := cli.Coupon.Get(ctx, *h.CouponID)
+		if err != nil {
+			return err
+		}
+		if coupon == nil {
+			return fmt.Errorf("coupon not found %v", *h.CouponID)
+		}
+		return nil
+	})
 }
 
 func (h *createHandler) createScope(ctx context.Context, cli *ent.Client) error {
@@ -44,6 +46,13 @@ func (h *createHandler) createScope(ctx context.Context, cli *ent.Client) error 
 }
 
 func (h *Handler) CreateScope(ctx context.Context) (*npool.Scope, error) {
+	handler := &createHandler{
+		Handler: h,
+	}
+	if err := handler.getCoupon(ctx); err != nil {
+		return nil, err
+	}
+
 	h.Conds = &scopecrud.Conds{
 		GoodID:      &cruder.Cond{Op: cruder.EQ, Val: *h.GoodID},
 		CouponID:    &cruder.Cond{Op: cruder.EQ, Val: *h.CouponID},
@@ -57,18 +66,12 @@ func (h *Handler) CreateScope(ctx context.Context) (*npool.Scope, error) {
 		return nil, fmt.Errorf("coupon scope %v already exist", *h.CouponScope)
 	}
 
-	handler := &createHandler{
-		Handler: h,
-	}
 	id := uuid.New()
 	if h.ID == nil {
 		h.ID = &id
 	}
 
 	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
-		if err := handler.verifyCoupon(ctx, cli); err != nil {
-			return err
-		}
 		if err := handler.createScope(ctx, cli); err != nil {
 			return err
 		}
