f0678f24cf2d2fc5eb30e4927dac1c5795bbc5cb
lidaqiang <dqli161@163.com>
2023-11-10 15:15:07 +0800

    add subscriber

---
+++ pkg/pubsub/coupon/allocated/allocated.go
@@ -0,0 +1,36 @@
+package allocated
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	"github.com/NpoolPlatform/go-service-framework/pkg/pubsub"
+	allocated1 "github.com/NpoolPlatform/inspire-middleware/pkg/mw/coupon/allocated"
+	allocatedmwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/coupon/allocated"
+)
+
+func Prepare(body string) (interface{}, error) {
+	req := []*allocatedmwpb.CouponReq{}
+	if err := json.Unmarshal([]byte(body), &req); err != nil {
+		return nil, err
+	}
+	return &req, nil
+}
+
+func Apply(ctx context.Context, req interface{}, publisher *pubsub.Publisher) error {
+	reqs := req.([]*allocatedmwpb.CouponReq)
+
+	handler, err := allocated1.NewHandler(
+		ctx,
+		allocated1.WithReqs(reqs, true),
+	)
+	if err != nil {
+		return err
+	}
+	if _, err := handler.UpdateCoupons(ctx); err != nil {
+		logger.Sugar().Errorf("update allocated coupons failed %v", err)
+		return err
+	}
+	return nil
+}
