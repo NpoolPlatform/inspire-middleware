21e2f4023c9e28c030e42aa23987c7867434d610
lidaqiang <dqli161@163.com>
2023-11-13 17:24:59 +0800

    add migrator of auto id

--- cmd/inspire-middleware/run.go
+++ cmd/inspire-middleware/run.go
@@ -9,6 +9,7 @@ import (
 	"github.com/NpoolPlatform/inspire-middleware/api"
 	"github.com/NpoolPlatform/inspire-middleware/pkg/db"
 	"github.com/NpoolPlatform/inspire-middleware/pkg/mw/invitation/registration"
+	"github.com/NpoolPlatform/inspire-middleware/pkg/migrator"
 
 	apicli "github.com/NpoolPlatform/basal-middleware/pkg/client/api"
 
@@ -37,6 +38,9 @@ var runCmd = &cli.Command{
 }
 
 func run(ctx context.Context) error {
+	if err := migrator.Migrate(ctx); err != nil {
+		return err
+	}
 	if err := db.Init(); err != nil {
 		return err
 	}
