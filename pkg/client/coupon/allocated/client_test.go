bfc6cd0cebd4edc2ea041d337c6d57c69969611c
lidaqiang <dqli161@163.com>
2023-11-14 06:04:14 +0000

    fix allocated ut

--- pkg/client/coupon/allocated/client_test.go
+++ pkg/client/coupon/allocated/client_test.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/config"
 
-	coupon "github.com/NpoolPlatform/inspire-middleware/pkg/client/coupon"
+	couponmwcli "github.com/NpoolPlatform/inspire-middleware/pkg/client/coupon"
 	couponmwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/coupon"
 	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/coupon/allocated"
 
@@ -39,7 +39,7 @@ func init() {
 	}
 }
 
-var ret = &couponmwpb.Coupon{
+var coupon = &couponmwpb.Coupon{
 	EntID:               uuid.NewString(),
 	CouponType:          types.CouponType_Discount,
 	CouponTypeStr:       types.CouponType_Discount.String(),
@@ -58,60 +58,51 @@ var ret = &couponmwpb.Coupon{
 	CouponScopeStr:      types.CouponScope_Whitelist.String(),
 }
 
-var req = &couponmwpb.CouponReq{
-	ID:           &ret.ID,
-	CouponType:   &ret.CouponType,
-	AppID:        &ret.AppID,
-	Denomination: &ret.Denomination,
-	Circulation:  &ret.Circulation,
-	IssuedBy:     &ret.IssuedBy,
-	StartAt:      &ret.StartAt,
-	DurationDays: &ret.DurationDays,
-	Message:      &ret.Message,
-	Name:         &ret.Name,
-}
-
-var ret1 = &npool.Coupon{
+var ret = &npool.Coupon{
 	EntID:               uuid.NewString(),
 	CouponType:          types.CouponType_Discount,
 	CouponTypeStr:       types.CouponType_Discount.String(),
-	AppID:               ret.AppID,
-	CouponID:            ret.EntID,
+	AppID:               coupon.AppID,
+	CouponID:            coupon.EntID,
 	UserID:              uuid.NewString(),
-	Denomination:        ret.Denomination,
-	Circulation:         ret.Circulation,
-	DurationDays:        ret.DurationDays,
-	CouponName:          ret.Name,
-	Message:             ret.Message,
+	Denomination:        coupon.Denomination,
+	Circulation:         coupon.Circulation,
+	DurationDays:        coupon.DurationDays,
+	CouponName:          coupon.Name,
+	Message:             coupon.Message,
 	CouponConstraint:    types.CouponConstraint_Normal,
 	CouponConstraintStr: types.CouponConstraint_Normal.String(),
 	CouponScope:         types.CouponScope_Whitelist,
 	CouponScopeStr:      types.CouponScope_Whitelist.String(),
 	Valid:               true,
-}
-
-var req1 = &npool.CouponReq{
-	ID:       &ret1.ID,
-	AppID:    &ret1.AppID,
-	CouponID: &ret1.CouponID,
-	UserID:   &ret1.UserID,
+	Allocated:           "1",
 }
 
 func createCoupon(t *testing.T) {
-	_, err := coupon.CreateCoupon(context.Background(), req)
+	_, err := couponmwcli.CreateCoupon(context.Background(), &couponmwpb.CouponReq{
+		EntID:        &coupon.EntID,
+		CouponType:   &coupon.CouponType,
+		AppID:        &coupon.AppID,
+		Denomination: &coupon.Denomination,
+		Circulation:  &coupon.Circulation,
+		IssuedBy:     &coupon.IssuedBy,
+		StartAt:      &coupon.StartAt,
+		DurationDays: &coupon.DurationDays,
+		Message:      &coupon.Message,
+		Name:         &coupon.Name,
+	})
 	assert.Nil(t, err)
 
-	info, err := CreateCoupon(context.Background(), req1)
+	info, err := CreateCoupon(context.Background(), &npool.CouponReq{
+		AppID:    &ret.AppID,
+		CouponID: &ret.CouponID,
+	})
 	if assert.Nil(t, err) {
-		ret1.ID = info.ID
-		ret1.CreatedAt = info.CreatedAt
-		ret1.UpdatedAt = info.UpdatedAt
-		ret1.StartAt = info.StartAt
-		ret1.EndAt = info.EndAt
-		ret1.Valid = info.Valid
-		ret.Allocated = decimal.NewFromInt(1).String()
-		ret1.Allocated = decimal.NewFromInt(1).String()
-		assert.Equal(t, ret1, info)
+		ret.ID = info.ID
+		ret.CreatedAt = info.CreatedAt
+		ret.UpdatedAt = info.UpdatedAt
+		assert.Equal(t, ret, info)
+		coupon.Allocated = info.Allocated
 	}
 }
 
@@ -119,48 +110,49 @@ func updateCoupon(t *testing.T) {
 	used := true
 	orderID := uuid.NewString()
 
-	ret1.Used = used
-	ret1.UsedByOrderID = &orderID
+	ret.Used = used
+	ret.UsedByOrderID = &orderID
 
-	req1.Used = &used
-	req1.UsedByOrderID = &orderID
-
-	info, err := UpdateCoupon(context.Background(), req1)
+	info, err := UpdateCoupon(context.Background(), &npool.CouponReq{
+		ID:            &ret.ID,
+		Used:          &ret.Used,
+		UsedByOrderID: ret.UsedByOrderID,
+	})
 	if assert.Nil(t, err) {
-		ret1.UpdatedAt = info.UpdatedAt
-		ret1.UsedAt = info.UsedAt
-		assert.Equal(t, ret1, info)
+		ret.UpdatedAt = info.UpdatedAt
+		ret.UsedAt = info.UsedAt
+		assert.Equal(t, ret, info)
 	}
 }
 
 func getCouponCoupon(t *testing.T) {
-	info, err := GetCoupon(context.Background(), ret1.EntID)
+	info, err := GetCoupon(context.Background(), ret.EntID)
 	if assert.Nil(t, err) {
-		assert.Equal(t, ret1, info)
+		assert.Equal(t, ret, info)
 	}
 }
 
 func getCouponCoupons(t *testing.T) {
 	infos, total, err := GetCoupons(context.Background(), &npool.Conds{
-		AppID:  &basetypes.StringVal{Op: cruder.EQ, Value: ret1.AppID},
-		UserID: &basetypes.StringVal{Op: cruder.EQ, Value: ret1.UserID},
+		AppID:  &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppID},
+		UserID: &basetypes.StringVal{Op: cruder.EQ, Value: ret.UserID},
 	}, int32(0), int32(100))
 	if assert.Nil(t, err) {
 		assert.Equal(t, total, uint32(1))
 		assert.Equal(t, len(infos), 1)
-		assert.Equal(t, ret1, infos[0])
+		assert.Equal(t, ret, infos[0])
 	}
 }
 
 func getCouponCouponOnly(t *testing.T) {
 	info, err := GetCouponOnly(context.Background(), &npool.Conds{
-		EntID:         &basetypes.StringVal{Op: cruder.EQ, Value: ret1.EntID},
-		AppID:         &basetypes.StringVal{Op: cruder.EQ, Value: ret1.AppID},
-		UserID:        &basetypes.StringVal{Op: cruder.EQ, Value: ret1.UserID},
-		UsedByOrderID: &basetypes.StringVal{Op: cruder.EQ, Value: ret1.GetUsedByOrderID()},
+		EntID:         &basetypes.StringVal{Op: cruder.EQ, Value: ret.EntID},
+		AppID:         &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppID},
+		UserID:        &basetypes.StringVal{Op: cruder.EQ, Value: ret.UserID},
+		UsedByOrderID: &basetypes.StringVal{Op: cruder.EQ, Value: ret.GetUsedByOrderID()},
 	})
 	if assert.Nil(t, err) {
-		assert.Equal(t, ret1, info)
+		assert.Equal(t, ret, info)
 	}
 }
 
