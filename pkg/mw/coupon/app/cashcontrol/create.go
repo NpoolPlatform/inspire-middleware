a99335bdc54f3a1ee514c4755a51f34dc8bf7028
lidaqiang <dqli161@163.com>
2024-01-18 11:41:07 +0800

    check coupon type

--- pkg/mw/coupon/app/cashcontrol/create.go
+++ pkg/mw/coupon/app/cashcontrol/create.go
@@ -7,7 +7,9 @@ import (
 	cashcontrolcrud "github.com/NpoolPlatform/inspire-middleware/pkg/crud/coupon/app/cashcontrol"
 	"github.com/NpoolPlatform/inspire-middleware/pkg/db"
 	"github.com/NpoolPlatform/inspire-middleware/pkg/db/ent"
+	coupon1 "github.com/NpoolPlatform/inspire-middleware/pkg/mw/coupon"
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	inspiretypes "github.com/NpoolPlatform/message/npool/basetypes/inspire/v1"
 	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/coupon/app/cashcontrol"
 
 	"github.com/google/uuid"
@@ -15,6 +17,31 @@ import (
 
 type createHandler struct {
 	*Handler
+}
+
+func (h *createHandler) getCoupon(ctx context.Context) error {
+	couponID := h.CouponID.String()
+	handler, err := coupon1.NewHandler(
+		ctx,
+		coupon1.WithEntID(&couponID, true),
+	)
+	if err != nil {
+		return err
+	}
+	coupon, err := handler.GetCoupon(ctx)
+	if err != nil {
+		return err
+	}
+	if coupon == nil {
+		return fmt.Errorf("invalid coupon")
+	}
+	if coupon.AppID != h.AppID.String() {
+		return fmt.Errorf("invalid coupon")
+	}
+	if coupon.CouponType != inspiretypes.CouponType_FixAmount {
+		return fmt.Errorf("invalid coupon type")
+	}
+	return nil
 }
 
 func (h *createHandler) createCashControl(ctx context.Context, cli *ent.Client) error {
@@ -36,6 +63,9 @@ func (h *createHandler) createCashControl(ctx context.Context, cli *ent.Client) 
 func (h *Handler) CreateCashControl(ctx context.Context) (*npool.CashControl, error) {
 	handler := &createHandler{
 		Handler: h,
+	}
+	if err := handler.getCoupon(ctx); err != nil {
+		return nil, err
 	}
 
 	h.Conds = &cashcontrolcrud.Conds{
