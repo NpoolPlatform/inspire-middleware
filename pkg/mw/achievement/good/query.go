147561a35ad4e25d7bcbd7ebd3857950b4858a2f
Zhao KK <kikakkz@hotmail.com>
2024-05-14 10:52:44 +0000

    Add good achievement query

---
+++ pkg/mw/achievement/good/query.go
@@ -0,0 +1,96 @@
+package goodachievement
+
+import (
+	"context"
+
+	wlog "github.com/NpoolPlatform/go-service-framework/pkg/wlog"
+	"github.com/NpoolPlatform/inspire-middleware/pkg/db"
+	"github.com/NpoolPlatform/inspire-middleware/pkg/db/ent"
+	entgoodachievement "github.com/NpoolPlatform/inspire-middleware/pkg/db/ent/goodachievement"
+	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/achievement/good"
+)
+
+type queryHandler struct {
+	*baseQueryHandler
+	stmCount *ent.GoodAchievementSelect
+	infos    []*npool.Achievement
+	total    uint32
+}
+
+func (h *queryHandler) scan(ctx context.Context) error {
+	return h.stmSelect.Scan(ctx, &h.infos)
+}
+
+func (h *queryHandler) formalize() {
+	// TODO
+}
+
+func (h *Handler) GetAchievement(ctx context.Context) (*npool.Achievement, error) {
+	handler := &queryHandler{
+		baseQueryHandler: &baseQueryHandler{
+			Handler: h,
+		},
+	}
+
+	err := db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		if err := handler.queryGoodAchievement(cli); err != nil {
+			return wlog.WrapError(err)
+		}
+		handler.queryJoin()
+		return handler.scan(_ctx)
+	})
+	if err != nil {
+		return nil, wlog.WrapError(err)
+	}
+	if len(handler.infos) == 0 {
+		return nil, nil
+	}
+	if len(handler.infos) > 1 {
+		return nil, wlog.Errorf("too many records")
+	}
+
+	handler.formalize()
+
+	return handler.infos[0], nil
+}
+
+func (h *Handler) GetAchievements(ctx context.Context) ([]*npool.Achievement, uint32, error) {
+	handler := &queryHandler{
+		baseQueryHandler: &baseQueryHandler{
+			Handler: h,
+		},
+	}
+
+	var err error
+	err = db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		handler.stmSelect, err = handler.queryGoodAchievements(cli)
+		if err != nil {
+			return wlog.WrapError(err)
+		}
+		handler.stmCount, err = handler.queryGoodAchievements(cli)
+		if err != nil {
+			return wlog.WrapError(err)
+		}
+
+		handler.queryJoin()
+		_total, err := handler.stmCount.Count(_ctx)
+		if err != nil {
+			return wlog.WrapError(err)
+		}
+		handler.total = uint32(_total)
+
+		handler.stmSelect.
+			Offset(int(h.Offset)).
+			Limit(int(h.Limit)).
+			Order(ent.Desc(entgoodachievement.FieldCreatedAt))
+
+		return handler.scan(_ctx)
+	})
+	if err != nil {
+		return nil, 0, err
+	}
+
+	handler.formalize()
+
+	return handler.infos, handler.total, nil
+}
