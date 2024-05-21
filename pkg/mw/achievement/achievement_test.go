63445481ecfb69cf3a3bb9bbb8b757edd7ee7a71
lidaqiang <dqli161@163.com>
2024-05-21 14:40:00 +0800

    fix ut

--- pkg/mw/achievement/achievement_test.go
+++ pkg/mw/achievement/achievement_test.go
@@ -8,11 +8,15 @@ import (
 	"strconv"
 	"testing"
 
-	statement1 "github.com/NpoolPlatform/inspire-middleware/pkg/mw/achievement/statement"
+	types "github.com/NpoolPlatform/message/npool/basetypes/inspire/v1"
+	orderstatementmwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/achievement/statement/order"
+	paymentmwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/achievement/statement/order/payment"
+
+	achievement1 "github.com/NpoolPlatform/inspire-middleware/pkg/mw/achievement/good"
+	statement1 "github.com/NpoolPlatform/inspire-middleware/pkg/mw/achievement/statement/order"
 	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
-	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/achievement"
-	statementmwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/achievement/statement"
+	npool "github.com/NpoolPlatform/message/npool/inspire/mw/v1/achievement/good"
 
 	"github.com/NpoolPlatform/inspire-middleware/pkg/testinit"
 
@@ -30,98 +34,70 @@ func init() {
 	}
 }
 
-var ret = &statementmwpb.Statement{
-	EntID:                  uuid.NewString(),
-	AppID:                  uuid.NewString(),
-	UserID:                 uuid.NewString(),
-	DirectContributorID:    uuid.NewString(),
-	GoodID:                 uuid.NewString(),
-	AppGoodID:              uuid.NewString(),
-	OrderID:                uuid.NewString(),
-	SelfOrder:              true,
-	PaymentID:              uuid.NewString(),
-	CoinTypeID:             uuid.NewString(),
-	PaymentCoinTypeID:      uuid.NewString(),
-	PaymentCoinUSDCurrency: "10.101",
-	Units:                  "10",
-	Amount:                 "10000",
-	USDAmount:              "20000",
-	Commission:             "300",
+var ret = &orderstatementmwpb.Statement{
+	EntID:                   uuid.NewString(),
+	AppID:                   uuid.NewString(),
+	UserID:                  uuid.NewString(),
+	GoodID:                  uuid.NewString(),
+	AppGoodID:               uuid.NewString(),
+	OrderID:                 uuid.NewString(),
+	OrderUserID:             uuid.NewString(),
+	GoodCoinTypeID:          uuid.NewString(),
+	Units:                   decimal.NewFromInt(10).String(),
+	GoodValueUSD:            decimal.NewFromInt(120).String(),
+	PaymentAmountUSD:        decimal.NewFromInt(120).String(),
+	CommissionAmountUSD:     decimal.NewFromInt(30).String(),
+	AppConfigID:             uuid.NewString(),
+	CommissionConfigID:      uuid.NewString(),
+	CommissionConfigType:    types.CommissionConfigType_LegacyCommissionConfig,
+	CommissionConfigTypeStr: types.CommissionConfigType_LegacyCommissionConfig.String(),
 }
 
 var ret1 = &npool.Achievement{
-	AppID:           ret.AppID,
-	UserID:          ret.UserID,
-	GoodID:          ret.GoodID,
-	AppGoodID:       ret.AppGoodID,
-	CoinTypeID:      ret.CoinTypeID,
-	TotalAmount:     ret.USDAmount,
-	SelfAmount:      ret.USDAmount,
-	TotalUnits:      ret.Units,
-	SelfUnits:       ret.Units,
-	TotalCommission: "3030.3",
-	SelfCommission:  "3030.3",
+	AppID:              ret.AppID,
+	UserID:             ret.UserID,
+	GoodID:             ret.GoodID,
+	AppGoodID:          ret.AppGoodID,
+	TotalAmountUSD:     ret.PaymentAmountUSD,
+	SelfAmountUSD:      "0",
+	TotalUnits:         ret.Units,
+	SelfUnits:          "0",
+	TotalCommissionUSD: ret.CommissionAmountUSD,
+	SelfCommissionUSD:  "0",
 }
 
-func setup(t *testing.T) func(*testing.T) {
-	h1, err := statement1.NewHandler(
+func createStatement(t *testing.T) {
+	paymentCoinTypeID := uuid.NewString()
+	coinUSDCurrency := "1.23"
+	amount := "1000"
+	handler, err := statement1.NewHandler(
 		context.Background(),
 		statement1.WithEntID(&ret.EntID, true),
 		statement1.WithAppID(&ret.AppID, true),
 		statement1.WithUserID(&ret.UserID, true),
-		statement1.WithDirectContributorID(&ret.DirectContributorID, true),
 		statement1.WithGoodID(&ret.GoodID, true),
 		statement1.WithAppGoodID(&ret.AppGoodID, true),
 		statement1.WithOrderID(&ret.OrderID, true),
-		statement1.WithSelfOrder(&ret.SelfOrder, true),
-		statement1.WithPaymentID(&ret.PaymentID, true),
-		statement1.WithCoinTypeID(&ret.CoinTypeID, true),
-		statement1.WithPaymentCoinTypeID(&ret.PaymentCoinTypeID, true),
-		statement1.WithPaymentCoinUSDCurrency(&ret.PaymentCoinUSDCurrency, true),
+		statement1.WithOrderUserID(&ret.OrderUserID, true),
+		statement1.WithGoodCoinTypeID(&ret.GoodCoinTypeID, true),
 		statement1.WithUnits(&ret.Units, true),
-		statement1.WithAmount(&ret.Amount, true),
-		statement1.WithUSDAmount(&ret.USDAmount, true),
-		statement1.WithCommission(&ret.Commission, true),
+		statement1.WithGoodValueUSD(&ret.GoodValueUSD, true),
+		statement1.WithPaymentAmountUSD(&ret.PaymentAmountUSD, true),
+		statement1.WithCommissionAmountUSD(&ret.CommissionAmountUSD, true),
+		statement1.WithAppConfigID(&ret.AppConfigID, true),
+		statement1.WithCommissionConfigID(&ret.CommissionConfigID, true),
+		statement1.WithCommissionConfigType(&ret.CommissionConfigType, true),
+		statement1.WithPaymentStatements([]*paymentmwpb.StatementReq{{
+			PaymentCoinTypeID: &paymentCoinTypeID,
+			CoinUSDCurrency:   &coinUSDCurrency,
+			Amount:            &amount,
+		},
+		}, true),
 	)
 	assert.Nil(t, err)
 
-	info, err := h1.CreateStatement(context.Background())
-	if assert.Nil(t, err) {
-		ret.ID = info.ID
-		h1.ID = &info.ID
-	}
-
-	return func(*testing.T) {
-		_, _ = h1.DeleteStatement(context.Background())
-	}
-}
-
-func getAchievements(t *testing.T) {
-	handler, err := NewHandler(
-		context.Background(),
-		WithConds(&npool.Conds{
-			AppID:      &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppID},
-			UserID:     &basetypes.StringVal{Op: cruder.EQ, Value: ret.UserID},
-			GoodID:     &basetypes.StringVal{Op: cruder.EQ, Value: ret.GoodID},
-			AppGoodID:  &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppGoodID},
-			CoinTypeID: &basetypes.StringVal{Op: cruder.EQ, Value: ret.CoinTypeID},
-			UserIDs:    &basetypes.StringSliceVal{Op: cruder.IN, Value: []string{ret.UserID}},
-		}),
-		WithLimit(0),
-	)
+	err = handler.CreateStatement(context.Background())
 	assert.Nil(t, err)
-
-	infos, total, err := handler.GetAchievements(context.Background())
-	if assert.Nil(t, err) {
-		assert.Equal(t, uint32(1), total)
-		if assert.Equal(t, 1, len(infos)) {
-			ret1.ID = infos[0].ID
-			ret1.EntID = infos[0].EntID
-			ret1.CreatedAt = infos[0].CreatedAt
-			ret1.UpdatedAt = infos[0].UpdatedAt
-			assert.Equal(t, infos[0], ret1)
-		}
-	}
 }
 
 func expropriateAchievement(t *testing.T) {
@@ -144,34 +120,30 @@ func expropriateAchievement(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Nil(t, info)
 
-	h2, err := NewHandler(
+	h2, err := achievement1.NewHandler(
 		context.Background(),
-		WithConds(&npool.Conds{
-			AppID:      &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppID},
-			UserID:     &basetypes.StringVal{Op: cruder.EQ, Value: ret.UserID},
-			GoodID:     &basetypes.StringVal{Op: cruder.EQ, Value: ret.GoodID},
-			AppGoodID:  &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppGoodID},
-			CoinTypeID: &basetypes.StringVal{Op: cruder.EQ, Value: ret.CoinTypeID},
-			UserIDs:    &basetypes.StringSliceVal{Op: cruder.IN, Value: []string{ret.UserID}},
+		achievement1.WithConds(&npool.Conds{
+			AppID:     &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppID},
+			UserID:    &basetypes.StringVal{Op: cruder.EQ, Value: ret.UserID},
+			GoodID:    &basetypes.StringVal{Op: cruder.EQ, Value: ret.GoodID},
+			AppGoodID: &basetypes.StringVal{Op: cruder.EQ, Value: ret.AppGoodID},
+			UserIDs:   &basetypes.StringSliceVal{Op: cruder.IN, Value: []string{ret.UserID}},
 		}),
-		WithLimit(0),
 	)
 	assert.Nil(t, err)
 
 	infos, total, err := h2.GetAchievements(context.Background())
 	if assert.Nil(t, err) {
 		assert.Equal(t, uint32(1), total)
-		if assert.Equal(t, 1, len(infos)) {
-			ret1.TotalAmount = decimal.NewFromInt(0).String()
-			ret1.SelfAmount = decimal.NewFromInt(0).String()
-			ret1.TotalUnits = decimal.NewFromInt(0).String()
-			ret1.SelfUnits = decimal.NewFromInt(0).String()
-			ret1.TotalCommission = decimal.NewFromInt(0).String()
-			ret1.SelfCommission = decimal.NewFromInt(0).String()
-			ret1.CreatedAt = infos[0].CreatedAt
-			ret1.UpdatedAt = infos[0].UpdatedAt
-			assert.Equal(t, infos[0], ret1)
-		}
+		ret1.TotalAmountUSD = decimal.NewFromInt(0).String()
+		ret1.SelfAmountUSD = decimal.NewFromInt(0).String()
+		ret1.TotalUnits = decimal.NewFromInt(0).String()
+		ret1.SelfUnits = decimal.NewFromInt(0).String()
+		ret1.TotalCommissionUSD = decimal.NewFromInt(0).String()
+		ret1.SelfCommissionUSD = decimal.NewFromInt(0).String()
+		ret1.CreatedAt = infos[0].CreatedAt
+		ret1.UpdatedAt = infos[0].UpdatedAt
+		assert.Equal(t, infos[0], ret1)
 	}
 }
 
@@ -180,9 +152,6 @@ func TestAchievement(t *testing.T) {
 		return
 	}
 
-	teardown := setup(t)
-	defer teardown(t)
-
-	t.Run("getAchievements", getAchievements)
+	t.Run("createStatement", createStatement)
 	t.Run("expropriateAchievement", expropriateAchievement)
 }
