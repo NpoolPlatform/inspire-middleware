22485ae6204ed49f6095f8ce32d59e0de8a5ced5
jakys <jakysing@163.com>
2024-09-14 17:44:01 +0800

    Fix http://47.116.78.131:10083/issues/4419

--- pkg/mw/coin/config/delete.go
+++ pkg/mw/coin/config/delete.go
@@ -8,6 +8,11 @@ import (
 	configcrud "github.com/NpoolPlatform/inspire-middleware/pkg/crud/coin/config"
 	"github.com/NpoolPlatform/inspire-middleware/pkg/db"
 	"github.com/NpoolPlatform/inspire-middleware/pkg/db/ent"
+	coinallocated1 "github.com/NpoolPlatform/inspire-middleware/pkg/mw/coin/allocated"
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+	coinallocatedmwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/coin/allocated"
+	"github.com/google/uuid"
 )
 
 type deleteHandler struct {
@@ -27,6 +32,28 @@ func (h *deleteHandler) deleteCoinConfig(ctx context.Context, cli *ent.Client) e
 	return nil
 }
 
+func (h *deleteHandler) checkCoinAllocated(ctx context.Context) error {
+	conds := &coinallocatedmwpb.Conds{
+		AppID:        &basetypes.StringVal{Op: cruder.EQ, Value: h.AppID.String()},
+		CoinConfigID: &basetypes.StringVal{Op: cruder.EQ, Value: h.EntID.String()},
+	}
+	handler, err := coinallocated1.NewHandler(
+		ctx,
+		coinallocated1.WithConds(conds),
+	)
+	if err != nil {
+		return wlog.WrapError(err)
+	}
+	exist, err := handler.ExistCoinAllocatedConds(ctx)
+	if err != nil {
+		return wlog.WrapError(err)
+	}
+	if exist {
+		return wlog.Errorf("exist coinallocated")
+	}
+	return nil
+}
+
 func (h *Handler) DeleteCoinConfig(ctx context.Context) error {
 	handler := &deleteHandler{
 		Handler: h,
@@ -39,6 +66,11 @@ func (h *Handler) DeleteCoinConfig(ctx context.Context) error {
 	}
 	if info == nil {
 		return nil
+	}
+	h.AppID = func() *uuid.UUID { s := uuid.MustParse(info.AppID); return &s }()
+	h.EntID = func() *uuid.UUID { s := uuid.MustParse(info.EntID); return &s }()
+	if err := handler.checkCoinAllocated(ctx); err != nil {
+		return err
 	}
 
 	h.ID = &info.ID
